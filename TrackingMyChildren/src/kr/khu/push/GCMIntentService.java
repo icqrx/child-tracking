729a7aa18b8eb43efaa46a21cb36268315d75ae6
huuquoc09 <huuquoc09@gmail.com>
2014-06-22 08:37:25 +0000

    update ui message from service usign broadcast reciever

--- TrackingMyChildren/src/kr/khu/push/GCMIntentService.java
+++ TrackingMyChildren/src/kr/khu/push/GCMIntentService.java
@@ -1,140 +1,86 @@
 package kr.khu.push;
 
 import kr.khu.activity.MainActivity;
+import kr.khu.activity.MainActivity.UpdateUI;
 import kr.khu.activity.R;
-import kr.khu.utils.Def;
-import android.app.Notification;
+import android.app.IntentService;
 import android.app.NotificationManager;
 import android.app.PendingIntent;
 import android.content.Context;
 import android.content.Intent;
+import android.os.Bundle;
+import android.support.v4.app.NotificationCompat;
 import android.util.Log;
 
-import com.google.android.gcm.GCMBaseIntentService;
+import com.google.android.gms.gcm.GoogleCloudMessaging;
 
-public class GCMIntentService extends GCMBaseIntentService {
+public class GCMIntentService extends IntentService {
+	public static final int NOTIFICATION_ID = 1;
+	private static final String TAG = GCMIntentService.class.getSimpleName();
+	public static final String GCM_MESSAGE = "gcm_message";
+	private NotificationManager mNotificationManager;
+	NotificationCompat.Builder builder;
 
-	private static final String TAG = "GCMIntentService";
-	
-	private Controller aController = null;
+	public GCMIntentService() {
+		super("GCMIntentService");
+	}
 
-    public GCMIntentService() {
-    	// Call extended class Constructor GCMBaseIntentService
-        super(Def.SENDER_ID);
-    }
+	@Override
+	protected void onHandleIntent(Intent intent) {
+        // update UI in main activity
+        Intent broadcastIntent = new Intent();
+        broadcastIntent.setAction(UpdateUI.PROCESS_UPDATE_UI);
+        broadcastIntent.addCategory(Intent.CATEGORY_DEFAULT);
+        //
+		Bundle extras = intent.getExtras();
+		GoogleCloudMessaging gcm = GoogleCloudMessaging.getInstance(this);
+		// The getMessageType() intent parameter must be the intent you received
+        // in your BroadcastReceiver.
+        String messageType = gcm.getMessageType(intent);
+        if (!extras.isEmpty()) {  // has effect of unparcelling Bundle
+            /*
+             * Filter messages based on message type. Since it is likely that GCM
+             * will be extended in the future with new message types, just ignore
+             * any message types you're not interested in, or that you don't
+             * recognize.
+             */
+            if (GoogleCloudMessaging.MESSAGE_TYPE_SEND_ERROR.equals(messageType)) {
+                sendNotification("Send error: " + extras.toString());
+            } else if (GoogleCloudMessaging. MESSAGE_TYPE_DELETED.equals(messageType)) {
+                sendNotification("Deleted messages on server: " + extras.toString());
+            // If it's a regular GCM message, do some work.
+            } else if (GoogleCloudMessaging.MESSAGE_TYPE_MESSAGE.equals(messageType)) {
+                // Post notification of received message.
+                sendNotification(extras.getString("price"));
+                Log.i(TAG, "Received: " + extras.toString());
+                //update ui
+                broadcastIntent.putExtra(GCM_MESSAGE, extras.getString("price").toString());
+                sendBroadcast(broadcastIntent);
+            }
+        }
+        // Release the wake lock provided by the WakefulBroadcastReceiver.
+        GcmBroadcastReceiver.completeWakefulIntent(intent);
 
-    /**
-     * Method called on device registered
-     **/
-    @Override
-    protected void onRegistered(Context context, String registrationId) {
-    	
-    	//Get Global Controller Class object (see application tag in AndroidManifest.xml)
-    	if(aController == null)
-           aController = (Controller) getApplicationContext();
-    	
-        Log.i(TAG, "Device registered: regId = " + registrationId);
-        aController.displayMessageOnScreen(context, "Your device registred with GCM");
-        //aController.register(context, MainActivity.name, MainActivity.email, registrationId);
-    }
-
-    /**
-     * Method called on device unregistred
-     * */
-    @Override
-    protected void onUnregistered(Context context, String registrationId) {
-    	if(aController == null)
-            aController = (Controller) getApplicationContext();
-        Log.i(TAG, "Device unregistered");
-        aController.displayMessageOnScreen(context, "unregister");
-        //aController.unregister(context, registrationId);
-    }
-
-    /**
-     * Method called on Receiving a new message from GCM server
-     * */
-    @Override
-    protected void onMessage(Context context, Intent intent) {
-    	
-    	if(aController == null)
-            aController = (Controller) getApplicationContext();
-    	
-        Log.i(TAG, "Received message");
-        String message = intent.getExtras().getString("price");
+       
         
-        aController.displayMessageOnScreen(context, message);
-        // notifies user
-        generateNotification(context, message);
-    }
-
-    /**
-     * Method called on receiving a deleted message
-     * */
-    @Override
-    protected void onDeletedMessages(Context context, int total) {
-    	
-    	if(aController == null)
-            aController = (Controller) getApplicationContext();
-    	
-        Log.i(TAG, "Received deleted messages notification");
-        String message = "mess deleted";
-        aController.displayMessageOnScreen(context, message);
-        // notifies user
-        generateNotification(context, message);
-    }
-
-    /**
-     * Method called on Error
-     * */
-    @Override
-    public void onError(Context context, String errorId) {
-    	
-    	if(aController == null)
-            aController = (Controller) getApplicationContext();
-    	
-        Log.i(TAG, "Received error: " + errorId);
-        aController.displayMessageOnScreen(context, "error");
-    }
-
-    @Override
-    protected boolean onRecoverableError(Context context, String errorId) {
-    	
-    	if(aController == null)
-            aController = (Controller) getApplicationContext();
-    	
-        // log message
-        Log.i(TAG, "Received recoverable error: " + errorId);
-        //aController.displayMessageOnScreen(context, getString(R.string.gcm_recoverable_error, errorId));
-        return super.onRecoverableError(context, errorId);
-    }
-
-    /**
-     * Create a notification to inform the user that server has sent a message.
-     */
-    private static void generateNotification(Context context, String message) {
-        int icon = R.drawable.ic_launcher;
-        long when = System.currentTimeMillis();
-        NotificationManager notificationManager = (NotificationManager)context.getSystemService(Context.NOTIFICATION_SERVICE);
-        Notification notification = new Notification(icon, message, when);
         
-        String title = context.getString(R.string.app_name);
-        
-        Intent notificationIntent = new Intent(context, MainActivity.class);
-        // set intent so it does not start a new activity
-        notificationIntent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP |Intent.FLAG_ACTIVITY_SINGLE_TOP);
-        PendingIntent intent = PendingIntent.getActivity(context, 0, notificationIntent, 0);
-        notification.setLatestEventInfo(context, title, message, intent);
-        notification.flags |= Notification.FLAG_AUTO_CANCEL;
-        
-        // Play default notification sound
-        notification.defaults |= Notification.DEFAULT_SOUND;
-        
-        //notification.sound = Uri.parse("android.resource://" + context.getPackageName() + "your_sound_file_name.mp3");
-        
-        // Vibrate if vibrate is enabled
-        notification.defaults |= Notification.DEFAULT_VIBRATE;
-        notificationManager.notify(0, notification);      
+	}
+	 // Put the message into a notification and post it.
+    // This is just one simple example of what you might choose to do with
+    // a GCM message.
+    private void sendNotification(String msg) {
+        mNotificationManager = (NotificationManager) this.getSystemService(Context.NOTIFICATION_SERVICE);
 
+        PendingIntent contentIntent = PendingIntent.getActivity(this, 0, new Intent(this, MainActivity.class), 0);
+        NotificationCompat.Builder mBuilder = new NotificationCompat.Builder(this)
+        .setSmallIcon(R.drawable.tracking_index)
+        .setContentTitle("TrackingChild Notification")
+        .setAutoCancel(true)
+        .setStyle(new NotificationCompat.BigTextStyle()
+        .bigText(msg))
+        .setContentText(msg);
+
+        mBuilder.setContentIntent(contentIntent);
+        mNotificationManager.notify(NOTIFICATION_ID, mBuilder.build());
     }
-
 }
